8560f7ade5078351caeb84cfe0ce5493393505f2
ViscLu <112127908+ViscLu@users.noreply.github.com>
2026-01-22 14:54:58 -0500

    Add score increase base on time value

--- Assets/UI/ScoreManager.cs
+++ Assets/UI/ScoreManager.cs
@@ -1,3 +1,4 @@
+using Unity.Hierarchy;
 using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
@@ -5,6 +6,14 @@ public class ScoreManager : MonoBehaviour
     public static ScoreManager Instance;
     public int score = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    // Timer value
+    public float baseRate = 0.1f;
+    public float accelPerRate = 0.15f;
+    public float maxRate = 50f;
+
+    public float elapsedTime;
+    public float accumulator;
 
     private void Awake()
     {
@@ -18,14 +27,37 @@ public class ScoreManager : MonoBehaviour
         }
     }
 
+    public void Update()
+    {
+        AddTimeScore();
+    }
+
     public void AddScore(int amount)
     {
         score += amount;
-        Debug.Log("Score: " + score);
+        //Debug.Log("Score: " + score);
+    }
+
+    void AddTimeScore()
+    {
+        elapsedTime += Time.deltaTime;
+        float rate = baseRate + (accelPerRate * elapsedTime * elapsedTime);
+        rate = Mathf.Min(rate, maxRate);
+
+        accumulator += rate * Time.deltaTime;
+        Debug.Log(accumulator);
+        if (accumulator >= 1f)
+        {
+            int pointsToAdd = Mathf.FloorToInt(accumulator);
+            score += pointsToAdd;
+            accumulator -= pointsToAdd;
+        }
     }
 
     public void ResetScore()
     {
         score = 0;
+        elapsedTime = 0f;
+        accumulator = 0f;
     }
 }
