3cbd203809498675a789040e362d9cbcac97fe45
ThorFrostick <145503610+ThorFrostick@users.noreply.github.com>
2026-01-25 14:27:14 -0500

    Fixed the rotation on the windmills
    The rotation speed can be set in the inspector

--- Assets/Scripts_Levels/Rotate_Z.cs
+++ Assets/Scripts_Levels/Rotate_Z.cs
@@ -1,16 +1,16 @@
+using JetBrains.Annotations;
 using UnityEngine;
 
-public class Windmill : MonoBehaviour
+public class Rotate_Z : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
+    //Set the speed of the rotation
+    [SerializeField]
+    private float rotationSpeed;
+    
     // Update is called once per frame
     void Update()
     {
-        
+        //Every frame, rotate along the z axis a small amount
+        transform.Rotate(new Vector3(0, 0, 1) * rotationSpeed * Time.deltaTime);
     }
 }
