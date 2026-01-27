7470d368eea159e8546f8758425d224800ab51fc
ViscLu <112127908+ViscLu@users.noreply.github.com>
2026-01-27 08:26:31 -0700

    Fix score scaling and cursor not showing on main scene

--- Assets/UI/StartMenu.cs
+++ Assets/UI/StartMenu.cs
@@ -3,6 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour
 {
+    void Start()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
     public void GoToScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
