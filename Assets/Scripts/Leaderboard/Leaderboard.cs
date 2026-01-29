e139d3c9d4c7a053ab433f742b23a97199d4e58d
ThorFrostick <145503610+ThorFrostick@users.noreply.github.com>
2026-01-29 14:47:47 -0500

    Added a way to call JSON data to the UI

--- Assets/Scripts/Leaderboard/Leaderboard.cs
+++ Assets/Scripts/Leaderboard/Leaderboard.cs
@@ -1,16 +1,45 @@
+using TMPro;
 using UnityEngine;
 
 public class Leaderboard : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    //The JSON file we will be reading and writing leaderboard data to/from.
+    [SerializeField]
+    private TextAsset json_file;
+
+    //The display text that holds the information from the JSON file.
+    private string leaderboard_data;
+
+    //The TextMeshPro object we will use to display the data from the JSON file.
+    [SerializeField]
+    private TextMeshProUGUI text_display;
+
+
+    private void Start()
     {
-        
+        LoadLeaderboard();
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Add the player's score to the JSON leaderboard.
+    /// </summary>
+    private void AddScore()
     {
+
+    }
+    
+    /// <summary>
+    /// Load data from a JSON object and display it in a text field.
+    /// </summary>
+    private void LoadLeaderboard()
+    {
+        //Initialize our leaderboard text.
+        leaderboard_data = "Scores:\n";
         
+        //Load our data from the JSON file, and pass it into our display string.
+        leaderboard_data += json_file.text;
+
+        //Display the leaderboard data on our Text object.
+        text_display.text = leaderboard_data;
     }
 }
