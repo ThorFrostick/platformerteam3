c6512715dfe62bf5da58100e0498dbfe254feadf
ThorFrostick <145503610+ThorFrostick@users.noreply.github.com>
2026-02-01 12:45:02 -0500

    Finally fixed up reading and writing to the JSON

--- Assets/Scripts/Leaderboard/Leaderboard.cs
+++ Assets/Scripts/Leaderboard/Leaderboard.cs
@@ -1,45 +1,80 @@
+using NUnit.Framework;
+using System;
 using TMPro;
 using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
 
 public class Leaderboard : MonoBehaviour
 {
-    //The JSON file we will be reading and writing leaderboard data to/from.
-    [SerializeField]
-    private TextAsset json_file;
+    //We will use a list of PlayerScores to store the data we get from the file.
+    private Scores scores;
 
-    //The display text that holds the information from the JSON file.
-    private string leaderboard_data;
-
-    //The TextMeshPro object we will use to display the data from the JSON file.
-    [SerializeField]
-    private TextMeshProUGUI text_display;
-
-
-    private void Start()
-    {
-        LoadLeaderboard();
-    }
-
-    /// <summary>
-    /// Add the player's score to the JSON leaderboard.
-    /// </summary>
-    private void AddScore()
-    {
-
-    }
     
-    /// <summary>
-    /// Load data from a JSON object and display it in a text field.
-    /// </summary>
-    private void LoadLeaderboard()
-    {
-        //Initialize our leaderboard text.
-        leaderboard_data = "Scores:\n";
-        
-        //Load our data from the JSON file, and pass it into our display string.
-        leaderboard_data += json_file.text;
+    private Scores loadedScores;
 
-        //Display the leaderboard data on our Text object.
-        text_display.text = leaderboard_data;
+    //Define the path we want to read from and write to
+    //private string path = Path.Combine(Application.persistentDataPath + "leaderboard.json");
+    public TextAsset file;
+
+    public void Start()
+    {
+        
+        LoadFile();
+
+        // -- Testing 
+        
+        
+        WriteFile(loadedScores.scores);
+    }
+
+    /// <summary>
+    /// Load JSON data from a specified path.
+    /// </summary>
+    public void LoadFile()
+    {
+        string jsonText = File.ReadAllText(AssetDatabase.GetAssetPath(file));
+
+        loadedScores = JsonUtility.FromJson<Scores>(jsonText);
+
+        //AddScore(loadedScores, 15);
+    }
+
+    /// <summary>
+    /// Write data to our JSON file.
+    /// </summary>
+    public void WriteFile(List<PlayerScores> players)
+    {
+        scores = new Scores();
+        scores.scores = players;
+
+        string json = JsonUtility.ToJson(scores, true);
+
+        File.WriteAllText(AssetDatabase.GetAssetPath(file), json);
+    }
+
+    public void AddScore(Scores scores, int currentCoins)
+    {
+        //Create a new score based on the coins recently acquired
+        PlayerScores newScore = new PlayerScores() { coins = currentCoins };
+
+        //Add the new score to the list of scores we have loaded in
+        scores.scores.Add(newScore);
+    }
+
+    /// <summary>
+    /// This class is the format we will use for uploading data to/from JSON files.
+    /// </summary>
+    [Serializable]
+    public class PlayerScores
+    {
+        public int coins;
+    }
+
+    [Serializable]
+    public class Scores
+    {
+        public List<PlayerScores> scores;
     }
 }
